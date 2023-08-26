2a0b6a0bf29d6baa5d9eebfc1d4bb2ef19857a04
Bruno Barreto da Cunha Holanda <bbarretoch@gmail.com>
2023-08-26 17:00:34 -0300

    Criada a requisicao POST

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,25 @@ async function listaVideos() {
     return conxexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "content-type" : "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        }) 
+    });
+    const conxexaoConvertida = await conexao.json();
+    return conxexaoConvertida;
+    
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
