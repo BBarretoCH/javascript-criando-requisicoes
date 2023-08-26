ec75fe85daffe385375bfc42309610ae8b162bfc
Bruno Barreto da Cunha Holanda <114125291+BBarretoCH@users.noreply.github.com>
2023-08-26 17:28:51 -0300

    Captando eventos (#4)
    * adicionado a tag script
    
    * Criada a função que constroi o card do video e a função que insere dinamicamente o video à pagina. Modificado o index.html para a exibição dos vídeos serem totalmente dinamicas
    
    * Criada a requisicao POST
    
    * Criado arquivo para adicionar video a partir do formulario

---
+++ js/criarVideo.js
@@ -0,0 +1,13 @@
+const formulario = document.querySelector('[data-formulario');
+
+function criarVideo(evento){
+    evento.preventDefault();
+    
+    const url = document.querySelector('[data-url');
+    const titulo = document.querySelector('[data-titulo');
+    const imagem = document.querySelector('[data-imagem');
+    const descricao = Math.floor(Math.random()*10).toString();
+     
+}
+
+formulario.addEventListener('submit', evento => criarVideo(evento));
