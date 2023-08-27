fc1eef80638162492fe14f7cf327cf3414f751ee
Bruno Barreto da Cunha Holanda <bbarretoch@gmail.com>
2023-08-26 22:04:56 -0300

    Tratamento de erros com try-catch, condicionais e array.length

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,29 +1,33 @@
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
     const conxexaoConvertida = await conexao.json();
-    
+
     return conxexaoConvertida
 }
 
-async function criaVideo(titulo, descricao, url, imagem){
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
-            "content-type" : "application/json"
+            "content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
             descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
-        }) 
+        })
     });
+
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
     const conxexaoConvertida = await conexao.json();
     return conxexaoConvertida;
-    
+
 }
 
-async function buscaVideo(termoDeBusca){
+async function buscaVideo(termoDeBusca) {
     const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
     const conxexaoConvertida = conexao.json();
 
